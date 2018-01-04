177ffb71bc99660d58160a2deac13ce423b25bcb
Nicholas Baron <njbaron@cpp.edu>
2018-01-04 13:35:44 -0800

    Basic Setup
    Write in the LoginSystem class.

---
+++ Student Login/src/LoginSystem.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class LoginSystem {
+
+	public static final File database = new File("database.txt");
+	
+	public static final Scanner userInput = new Scanner(System.in);
+	
+	
+	//Ask the user if they are registered.
+	public static void main(String[] args) {
+		
+
+	}
+
+	public void registerUser() {}
+	
+	
+	public boolean isInDataBase(String name, int id) {
+		
+	}
+	
+}
